57e1a5690886b8b1a6592835ff45bdd602ed4922
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-03-11 15:00:39 +0100

    positionning

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -5,6 +5,22 @@ const Scene = () => {
     return (
       <section title="Scene d'animation" className="scene-wrapper">
         <div className='animation animation-eiffel'></div>
+        <div className="animation animation-pinkWall"></div>
+        <div className="animation animation-pinkRoof"></div>
+        <div className="animation animation-orangeTree"></div>
+        <div className="animation animation-greenBuilding"></div>
+        <div className="animation animation-purplePlant"></div>
+        <div className="animation animation-redPlant"></div>
+        <div className="animation animation-leftBarrier"></div>
+        <div className="animation animation-treeSmall"></div>
+        <div className="animation animation-smallOrangeTree"></div>
+        <div className="animation animation-pinkShop"></div>
+        <div className="animation animation-bookShell"></div>
+        <div className="animation animation-stairs"></div>
+        <div className="animation animation-deskBuilding"></div>
+        <div className="animation animation-roof"></div>
+        <div className="animation animation-longLight"></div>
+        <div className="animation animation-shortLight"></div>
       </section>
     );
 };
